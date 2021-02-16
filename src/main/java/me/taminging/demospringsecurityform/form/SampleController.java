bfd21039faad69d5d32942c6bb92cf7bef431850
taemin <taminging@kakao.com>
2021-02-16 23:56:48 +0900

    Sample Page and Controller

---
+++ src/main/java/me/taminging/demospringsecurityform/form/SampleController.java
@@ -0,0 +1,36 @@
+package me.taminging.demospringsecurityform.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/")
+    public String index(Model model, Principal principal){
+        if(principal == null)
+            model.addAttribute("messages", "Hello Spring Security");
+        else
+            model.addAttribute("messages", "Hello,"  + principal.getName());
+
+        return "index";
+    }
+    @GetMapping("/info")
+    public String info(Model model){
+        model.addAttribute("messages", "info");
+        return "info";
+    }
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal){
+        model.addAttribute("messages", "Hello " + principal.getName());
+        return "dashboard";
+    }
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal){
+        model.addAttribute("messages", "Hello Admin, " + principal.getName());
+        return "admin";
+    }
+}
