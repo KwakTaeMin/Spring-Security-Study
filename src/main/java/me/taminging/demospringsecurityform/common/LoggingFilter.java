c4e9f53b12dc7c47118b0f4546143b31583db97e
taemin <taminging@kakao.com>
2021-03-12 23:40:04 +0900

    Custom filter add2

--- src/main/java/me/taminging/demospringsecurityform/common/LoggingFilter.java
+++ src/main/java/me/taminging/demospringsecurityform/common/LoggingFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 
@@ -20,7 +21,7 @@ public class LoggingFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+        stopWatch.start(((HttpServletRequest)servletRequest).getRequestURI());
 
         filterChain.doFilter(servletRequest, servletResponse);
 
